317484eb8068eb26d146b61ccf2b65c3fa9edacd
Mudassar Hanif <110333334+Mudassar-space@users.noreply.github.com>
2023-10-02 21:37:45 +0500

    refactor code

--- apis/schemas.py
+++ apis/schemas.py
@@ -3,16 +3,11 @@ from unicodedata import category
 from pydantic import BaseModel, EmailStr, Field
 from datetime import datetime
 
-# from sqlalchemy import DateTime
-
-# format = "%Y-%m-%dT%H:%M:%S.%fZ"
 
 class Product(BaseModel):
     name : str
     description : str
     price : int
-    
-
     
     
 class ProductResponse(BaseModel) :
@@ -39,55 +34,10 @@ class ProductSale(BaseModel):
     quantity : int 
 
 
-
-class Cate_update_response(BaseModel):
-    created_on: datetime
-    name: str
-  
-
-class User_response(BaseModel):
-    
-    name: str
-    email: str
-    user_id: int
-    created_at: str 
-    
-
-class Account(BaseModel):
-    user_id: int
-    account_name: str
-    initial_balance: int
-    type: str
-    created_at: datetime
-    
-    
-class Account_response(BaseModel):
-    user_id:int 
-    account_id: int
-    initial_balance: int
-    created_at: datetime
-    type: str
-    account_name: str
-
-
-
-    
-
 class Responses(BaseModel):
     
     status: bool
     message: str
-    
-
-class Ledger(BaseModel):
-    
-    amount: int
-    account_id: int
-    category_Id: int
-    user_id: int
-    transaction: str
-    transfer_to: str
-    created_at: datetime
     
 
     
